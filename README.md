bc5258acfb7dd78f1d9a597822420df7fc6495d9
Markus Fenske <iblue@gmx.net>
2014-04-28 20:40:54 +0200

    Initial

---
+++ README.md
@@ -0,0 +1,22 @@
+# New Universal Time (NUT)
+
+A new time format for the world.
+
+## Concept
+
+We are using the following units:
+
+1 day: 86400 (that is 24*60*60 seconds)
+1 year: 365 days (or 365 days in a leap year)
+
+A year begins at the day of the vernal equinox on 0 UTC (in the next few
+years, the date is march 20th).
+
+A day begins at 0:00 UTC.
+
+The current year is 2014.
+
+The date is given by the number of the year followed by a space followed
+by the number of the current day.
+
+The time of the day is given in ks (kiloseconds).
---
+++ nut.rb
@@ -0,0 +1,46 @@
+require 'date'
+
+class NutTime
+  def initialize(time = Time.now)
+    @time = time.utc
+  end
+
+  def time
+    "%02.03f" % ((@time.hour*3600 + @time.min*60 + @time.sec).to_f/1000)
+  end
+
+  def day
+    (@time.to_date - last_vernal_equinox.to_date).to_i
+  end
+
+  def year
+    last_vernal_equinox.year
+  end
+
+  def to_s
+    "#{year} #{day} #{time}"
+  end
+
+  def inspect
+    "#<#{self.class.to_s}: #{to_s}>"
+  end
+
+  private
+
+  def last_vernal_equinox
+    year = @time.year
+    lve  = Time.new(year, 3, 20)
+
+    if lve > @time
+     lve = Time.new(year - 1, 3, 20)
+    end
+
+    lve
+  end
+end
+
+class Time
+  def nut
+    NutTime.new(self)
+  end
+end
